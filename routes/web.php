bcd1b5d855306959d627fb350519213cd8776854
seiis <seiisland@gmail.com>
2025-01-23 10:42:13 +0900

    Renderデプロイのための設定

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use App\Http\Controllers\MemoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//本番環境
+Route::get('/', [App\Http\Controllers\MemoController::class, 'index']);
 
 Route::resource("memos", MemoController::class);
 Route::resource('memos', MemoController::class);
